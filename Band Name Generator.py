2bd07c9fbb3dc1c68f85b3b6c7b28da449d3ddb5
Abhishek <rautela.abhishek@pm.me>
2022-01-30 21:47:00 +0800

    Delete Band Name Generator.py

--- Band Name Generator.py
+++
@@ -1,5 +0,0 @@
-# Day 1 
-print("Time to generate your band name.")
-city = input("What city did you grow up in?\n")
-pet = input("What was the name of a pet?\n")
-print("Your band name is: " + city + " " + pet)
