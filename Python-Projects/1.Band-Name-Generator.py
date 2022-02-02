c9b13100163254a89efb82bb7fe034f713ecf47d
Abhishek <rautela.abhishek@pm.me>
2022-02-02 16:56:45 +0800

    Delete Python-Projects directory

--- Python-Projects/1.Band-Name-Generator.py
+++
@@ -1,4 +0,0 @@
-print("Time to generate your band name.")
-city = input("What city did you grow up in?\n")
-pet = input("What was the name of a pet?\n")
-print("Your band name is: " + city + " " + pet)
--- Python-Projects/2.Tip Calculator.py
+++
@@ -1,6 +0,0 @@
-print ("Welcome to the Tip Calculator")
-total = float(input("What was the total bill? $"))
-percentage = int(input("What percentage would you like to tip? "))
-ppl = int(input("How many people are splitting the bill? "))
-tpp = ( total * (1 + percentage/100) )/ppl
-print(f"Each person should should pay ${tpp:.2f}.")
