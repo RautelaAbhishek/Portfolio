eec8b6a0852f13bb33bbe7d5089c408a2cd1ca20
Abhishek <rautela.abhishek@pm.me>
2022-04-13 13:14:53 +0800

    Update 13.Coffee-Machine.py

--- Projects/13.Coffee-Machine.py
+++ Projects/13.Coffee-Machine.py
@@ -2,13 +2,12 @@ import os
 
 
 def collect_money(cost):
-    print('Please insert coins.')
+    print(f'The cost of your drink is ${"{0:.2f}".format(cost)}. Please insert coins.')
     quarters = int(input('How many quarters?: '))
     dimes = int(input('How many dimes?: '))
     nickels = int(input('How many nickels?: '))
     pennies = int(input('How many pennies?: '))
     money = (quarters * .25) + (dimes * .10) + (nickels * .05) + (pennies * .01)
-    print(money)
     change = (money - cost)
     if change < 0 :
         print('Sorry thats not enough money. Money Refunded')
@@ -18,46 +17,74 @@ def collect_money(cost):
 
 
 
-os.system('clear')
+os.system('cls')
 
-espresso_cost = 1.5
-latte_cost = 2.5
+
+
+espresso_cost = 1.50
+latte_cost = 2.50
 coffee_cost = 3
 
-water = 1000
-milk = 1000
-coffee = 1000
+water = 500
+milk = 500
+coffee = 100
 money = 0
 
 while True:
+    apologise = 'Sorry there is not enough of the following'
+
     drink = input(f"What would you like (Espresso/Latte/Coffee)\n").lower()
 
     if drink == 'report':
-        print(f'Water:  {water}ml \nMilk:   {milk}ml \nCoffee: {coffee}g \nMoney:  ${money}\n')
+        print(f'\nWater:  {water}ml \nMilk:   {milk}ml \nCoffee: {coffee}g \nMoney:  ${"{0:.2f}".format(money)}\n')
     elif drink == 'off':
         break
     elif drink == 'espresso':
         if water >= 50 and coffee >= 18:
+            collect_money(espresso_cost)
             water -= 50
             coffee -= 18
+            money += espresso_cost
+            print(f"Here is your Espresso.\n")
+        else:
+            if water < 50:
+                apologise += '\n- water'
+            if coffee < 18:
+                apologise += '\n- coffee'
+            print(apologise)
     elif drink == 'latte':
         if water >= 200 and coffee >= 24 and milk >= 150:
+            collect_money(latte_cost)
             water -= 200
             coffee -= 24
             milk -= 150
+            money += latte_cost
+            print("Here is your Latte.\n")
+        else:
+            if water < 200:
+                apologise += '\n- water'
+            if coffee < 24:
+                apologise += '\n- coffee'
+            if milk < 150:
+                apologise += '\n- milk'
+            print(apologise)
     elif drink == 'coffee':
         if water >= 250 and coffee >= 24 and milk >= 100:
             collect_money(coffee_cost)
             water -= 250
             coffee -= 24
             milk -= 100
-        if water < 250:
-            print('Sorry there is not enough Water')
-        if coffee < 24:
-            print('Sorry there is not enough Coffee')
-        if milk < 100:
-            print('sorry there is not enough Milk')
-            
+            money += coffee_cost
+            print("Here is your Coffee.\n")
+        else:
+            if water < 250:
+                apologise += '\n- water'
+            if coffee < 24:
+                apologise += '\n- coffee'
+            if milk < 100:
+                apologise += '\n- milk'
+            print(apologise)
+
         
 
 
