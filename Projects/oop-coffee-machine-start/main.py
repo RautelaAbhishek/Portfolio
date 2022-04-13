8a49b399f2837984805cd7321b747985f2eccf5c
Abhishek <rautela.abhishek@pm.me>
2022-04-13 19:20:59 +0800

    Add project

---
+++ Projects/oop-coffee-machine-start/main.py
@@ -0,0 +1,27 @@
+from operator import truediv
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+import os
+
+os.system('cls')
+
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+menu = Menu()
+
+while True:
+    options = menu.get_items()
+    choice = input(f"What would you like? ({options}): ")
+    
+    if choice == 'off':
+        break
+    elif choice == 'report':
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(choice)
+        if coffee_maker.is_resource_sufficient(drink):
+            if money_machine.make_payment(drink.cost):
+                coffee_maker.make_coffee(drink)
+            
